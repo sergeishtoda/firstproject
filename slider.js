1b8bb9d5101fc33c4af18479ed379a7b8dbef918
sergeishtoda <46109151+sergeishtoda@users.noreply.github.com>
2019-01-24 20:06:32 +0300

    Add files via upload
    slider

---
+++ slider.js
@@ -0,0 +1,16 @@
+var slr = document.getElementById("slider_right");
+var sll = document.getElementById("slider_left");
+
+slr.addEventListener("click", moveright);
+sll.addEventListener("click", moveleft);
+
+function moveright() {
+    document.getElementsByClassName("slides")[0].style.transform = "translateX(-400px)";
+
+  }
+
+function moveleft() {
+    document.getElementsByClassName("slides")[0].style.transform = "translateX(0px)";
+
+  }
+
